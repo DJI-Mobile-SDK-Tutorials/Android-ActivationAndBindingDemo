3da471be4779f43102f0793fbb2f4e200a42d726
Oliver Ou <oliver.ou@dji.com>
2018-01-11 16:43:12 +0800

    update MApplication.java file.

--- ActivationAndBindingDemo/app/src/main/java/com/dji/activationDemo/MApplication.java
+++ ActivationAndBindingDemo/app/src/main/java/com/dji/activationDemo/MApplication.java
@@ -5,24 +5,23 @@ import android.content.Context;
 
 import com.secneo.sdk.Helper;
 
-
 public class MApplication extends Application {
 
-    private DemoApplication fpvDemoApplication;
+    private DemoApplication demoApplication;
     @Override
     protected void attachBaseContext(Context paramContext) {
         super.attachBaseContext(paramContext);
         Helper.install(MApplication.this);
-        if (fpvDemoApplication == null) {
-            fpvDemoApplication = new DemoApplication();
-            fpvDemoApplication.setContext(this);
+        if (demoApplication == null) {
+            demoApplication = new DemoApplication();
+            demoApplication.setContext(this);
         }
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
-        fpvDemoApplication.onCreate();
+        demoApplication.onCreate();
     }
 
 }
